b4e4c6425d851b3eeaf8758420ae1f38f6fc9dfc
NunesDevelloper <arthurnunescarvalho2@gmail.com>
2026-03-01 19:00:41 -0300

    first commit back end

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace gerenciador_cahves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("abcdefghijklmnopqrstuvwyxz");
+        }
+    }
+}
