6623f592d400f0924aa6dc197b2a47379a6783c0
Guilherme <guimcvilmar@gmail.com>
2026-03-04 11:00:45 -0300

    Separação das pages

--- Dashboard.xaml.cs
+++ Dashboard.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,14 +16,13 @@ using System.Windows.Shapes;
 namespace Keys_manager___Tester
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interação lógica para Dashboard.xam
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Dashboard : Page
     {
-        public MainWindow()
+        public Dashboard()
         {
-            MainFrame.Navigate(new LoginPage());
-            
+            InitializeComponent();
         }
     }
 }
